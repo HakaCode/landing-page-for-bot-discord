d0f29ad95a7230d9673e9a46c096c1216e5cdad4
William Sandres <33907565+HakaCode@users.noreply.github.com>
2023-08-29 03:58:47 -0300

    :bento: add javascript file

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
+
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+});
